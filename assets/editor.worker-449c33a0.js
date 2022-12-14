40b7a9a7d893f1c3aa2dcf6b164d1b7f34402cf9
Renovamen <Renovamen@users.noreply.github.com>
2022-12-14 02:08:23 +0000

    deploy: e47b8fb9ab2dd6048b3436d312ba14c3bff729c9

---
+++ assets/editor.worker-449c33a0.js
@@ -0,0 +1,1 @@
+function r(){return new Worker("/assets/editor.worker-af80a56b.js")}export{r as default};
--- assets/editor.worker.fd687d02.js
+++
@@ -1,1 +0,0 @@
-function r(){return new Worker("/assets/editor.worker.b9a77fdc.js")}export{r as default};
