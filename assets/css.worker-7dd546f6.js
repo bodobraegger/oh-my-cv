f1ae01c9d2d70ed073266131badbe75419c8ba31
Renovamen <Renovamen@users.noreply.github.com>
2023-09-27 01:54:18 +0000

    deploy: 64f676396633aa3edd873770c938694d1c593ef6

--- assets/css.worker-7dd546f6.js
+++
@@ -1,1 +0,0 @@
-function r(){return new Worker("/assets/css.worker-8534ef4f.js")}export{r as default};
--- assets/editor.worker-bfa13202.js
+++
@@ -1,1 +0,0 @@
-function r(){return new Worker("/assets/editor.worker-9a091e5f.js")}export{r as default};
